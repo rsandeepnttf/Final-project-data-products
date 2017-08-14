f8ce0bf7c3cba1cbfc925ce54f65a611891680d5
sandeep <rsandeepnttf@gmail.com>
2017-08-14 08:59:43 +0530

    Add files via upload

---
+++ server.R
@@ -0,0 +1,47 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+shinyServer(function(input, output) {
+  output$distPlot <- renderPlot({
+    if(input$x=='a'){
+      i<-1
+    }
+    
+    if(input$x=='b'){
+      i<-2
+    }
+    
+    if(input$x=='c'){
+      i<-3
+    }
+    
+    if(input$x=='d'){
+      i<-4
+    }
+    
+    if(input$y=='e'){
+      j<-1
+    }
+    
+    if(input$y=='f'){
+      j<-2
+    }
+    
+    if(input$y=='g'){
+      j<-3
+    }
+    
+    if(input$y=='h'){
+      j<-4
+    }
+    
+    s    <- iris[, i]
+    k    <- iris[, j]
+    plot(s,k)
+  })
+})
